5f7892c3223a8428aa30129272f5e1528e92aeaf
Даниил Камский <toolumini@mail.ru>
2022-07-04 20:08:19 +0300

    1

--- RandomPassGenerator/Form1.cs
+++ RandomPassGenerator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace RandomPassGenerator
 {
     public partial class Form1 : Form
     {
+        StreamWriter writ = new StreamWriter("passtext.txt",true);
         Random r = new Random();
         static string CharArray = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?`~";
         char[] ChArray = CharArray.ToCharArray();
@@ -30,6 +32,7 @@ namespace RandomPassGenerator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             pgencount++;
             richTextBox1.Text = "";
             for(int i =0; i < count; i++)
@@ -37,6 +40,8 @@ namespace RandomPassGenerator
                 richTextBox1.Text += ChArray[r.Next(0, ChArray.Length)];
             }
             ChList.Add(richTextBox1.Text);
+            writ.WriteLine(richTextBox1.Text);
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -65,5 +70,11 @@ namespace RandomPassGenerator
                 richTextBox1.Text = ChList[pgencount];
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(richTextBox1.Text);
+        }
+        
     }
 }
