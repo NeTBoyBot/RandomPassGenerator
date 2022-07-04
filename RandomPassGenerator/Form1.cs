3a51f23a228e3e00abd744c2e66fcbccef570e00
Даниил <@LAPTOP-U3B5GFL1>
2022-07-04 10:12:37 +0300

    Добавьте файлы проекта.

---
+++ RandomPassGenerator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPassGenerator
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        static string CharArray = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?`~";
+        char[] ChArray = CharArray.ToCharArray();
+        int count;
+        int pgencount=0;
+        List<string> ChList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            count = Convert.ToInt32(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pgencount++;
+            richTextBox1.Text = "";
+            for(int i =0; i < count; i++)
+            {
+                richTextBox1.Text += ChArray[r.Next(0, ChArray.Length)];
+            }
+            ChList.Add(richTextBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pgencount >= ChList.Count-1)
+            {
+                richTextBox1.Text = ChList[ChList.Count-1];
+            }
+            else 
+            {
+                pgencount++;
+                richTextBox1.Text = ChList[pgencount];
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pgencount <=0)
+            {
+                richTextBox1.Text = ChList[0];
+            }
+            else
+            {
+                pgencount--;
+                richTextBox1.Text = ChList[pgencount];
+            }
+        }
+    }
+}
